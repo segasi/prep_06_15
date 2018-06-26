d0988f8a91eb6478b0a3c8bb8ec699488bc61b27
segasi <segasi@gmail.com>
2018-06-26 16:14:38 -0500

    Cálculo y gráfica de paquetes sin acta en casillas con una, tres o cuatro elecciones procesadas en 2015

--- 02_codigo/cargar_limpiar_datos.R
+++ 02_codigo/cargar_limpiar_datos.R
@@ -166,3 +166,44 @@ p_2015 <- p_2015 %>%
                              estado == "TABASCO" ~ 3,
                              estado == "YUCATÁN" ~ 3),
          num_act = ifelse(is.na(num_act), 1, num_act))
+
+### Cálculo y gráfica de paquetes sin acta en casillas con una, tres o cuatro elecciones procesadas en 2015 ----
+
+# Cálculo
+p_2015 %>% 
+  mutate(observaciones_bis = ifelse(str_detect(observaciones, "Sin acta"), "Sin acta", as.character(observaciones))) %>% 
+  group_by(num_act, observaciones_bis) %>% 
+  tally() %>% 
+  ungroup() %>% 
+  group_by(num_act) %>% 
+  mutate(por = (n/sum(n)*100)) %>% 
+  ungroup() 
+
+
+# Gráfica
+p_2015 %>% 
+  mutate(observaciones_bis = ifelse(str_detect(observaciones, "Sin acta"), "Sin acta", as.character(observaciones))) %>% 
+  group_by(num_act, observaciones_bis) %>% 
+  tally() %>% 
+  ungroup() %>% 
+  group_by(num_act) %>% 
+  mutate(por = round((n/sum(n)*100), 1)) %>% 
+  ungroup() %>% 
+  filter(observaciones_bis == "Sin acta") %>% 
+  mutate(num_act = as.character(num_act), 
+         num_act = recode(num_act, "1" = "Un acta", "3" = "Tres actas", "4" = "Cuatro actas"),
+         num_act = fct_relevel(num_act, "Un acta", "Tres actas", "Cuatro actas")) %>% 
+  ggplot(aes(num_act, por)) +
+  geom_col(fill = "steelblue") +
+  geom_text(aes(label = paste(por, "%", sep = "")), size = 7, fontface = "bold", vjust = 1.5, col = "white") +
+  labs(title = "% DE PAQUETES ELECTORALES SIN ACTA PREP DE ACUERDO CON EL NÚMERO DE ACTAS\nPROCESADAS EN CASILLA | PREP 2015", 
+       x = "",
+       y = "",
+       caption = "Sebastián Garrido de Sierra / @segasi / Fuente: INE") +
+  tema +
+  theme(axis.text.y = element_blank())
+
+
+ggsave(filename = paste("2015_paquetes_sin_acta_prep_por_num_actas_procesadas.png", sep = ""), path = "03_graficas/actas_capturadas/", width = 15, height = 10, dpi = 100)
+
+
