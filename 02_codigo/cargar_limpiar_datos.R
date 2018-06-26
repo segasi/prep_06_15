3a1915bbc1e796d1656ef4879f5abe1a272009df
segasi <segasi@gmail.com>
2018-06-26 15:46:51 -0500

    Iniciar análisis

---
+++ 02_codigo/cargar_limpiar_datos.R
@@ -0,0 +1,12 @@
+### Paquetes ----
+library(pacman)
+p_load(animation, cowplot, extrafont, forcats, ggforce, gganimate, 
+       ggrepel, ggridges, janitor, lubridate, purrr, 
+       readxl, scales, stringi, stringr, tweenr, 
+       tidyr, tidyverse, treemapify, zoo)
+
+Sys.setlocale("LC_ALL", "es_ES.UTF-8") 
+options(scipen = 9999)
+
+
+
